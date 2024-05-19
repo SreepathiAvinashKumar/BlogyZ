66838c5f7feb0988ef0f6abe9ac544822ddf4712
Sripathi Avinash Kumar <65992275+SreepathiAvinashKumar@users.noreply.github.com>
2024-05-19 12:43:55 +0530

    Update card_hover_effect.js
    Card Details position, changed to before return

--- src/app/components/ui/card_hover_effect.js
+++ src/app/components/ui/card_hover_effect.js
@@ -25,6 +25,41 @@ function stripHtmlTags(html) {
   return tempElement.textContent || tempElement.innerText || '';
 }
 
+  const Card = ({ className, children }) => {
+  return (
+    <div
+      className={cn(
+        "rounded-2xl h-full w-full p-3 overflow-hidden bg-black border border-transparent dark:border-white/[0.2] group-hover:border-slate-700 relative z-20",
+        className
+      )}
+    >
+      <div className="relative z-50">
+        <div className="p-3">{children}</div>
+      </div>
+    </div>
+  );
+};
+
+const CardTitle = ({ className, children }) => {
+  return (
+    <h5 className={cn("text-zinc-100 font-bold tracking-wide mt-4", className)}>
+      {children}
+    </h5>
+  );
+};
+
+const CardDescription = ({ className, children }) => {
+  return (
+    <p
+      className={cn(
+        "mt-8 text-zinc-400 tracking-wide leading-relaxed text-sm",
+        className
+      )}
+    >
+      {children}
+    </p>
+  );
+};
 
   return (
     <div
@@ -64,40 +99,6 @@ function stripHtmlTags(html) {
   );
 };
 
-const Card = ({ className, children }) => {
-  return (
-    <div
-      className={cn(
-        "rounded-2xl h-full w-full p-3 overflow-hidden bg-black border border-transparent dark:border-white/[0.2] group-hover:border-slate-700 relative z-20",
-        className
-      )}
-    >
-      <div className="relative z-50">
-        <div className="p-3">{children}</div>
-      </div>
-    </div>
-  );
-};
 
-const CardTitle = ({ className, children }) => {
-  return (
-    <h5 className={cn("text-zinc-100 font-bold tracking-wide mt-4", className)}>
-      {children}
-    </h5>
-  );
-};
-
-const CardDescription = ({ className, children }) => {
-  return (
-    <p
-      className={cn(
-        "mt-8 text-zinc-400 tracking-wide leading-relaxed text-sm",
-        className
-      )}
-    >
-      {children}
-    </p>
-  );
-};
 
 export { HoverEffect};
